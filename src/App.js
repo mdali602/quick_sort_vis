89ad1f612b8e93286c32f26b011ce0ada4729950
Mohd Ali <mdali602@gmail.com>
2019-08-28 09:17:17 +0530

    merged

--- src/App.js
+++ src/App.js
@@ -9,23 +9,16 @@ const swap = (arr, firstIndex, secondIndex) => {
   arr[secondIndex] = temp
 }
 
-
-/* const theLoop  = (i) => {
-  setTimeout(() => {
-    console.log("Cheese! ", i);
-    if (--i) {          // If i > 0, keep going
-      theLoop(i);       // Call the loop again, and pass it the current value of i
-    }
-  }, 3000);
-}; */
-
 class App extends Component {
-  count_pause = 0;
-  count_resume = 0;
   state = {
     err_msg: '',
+<<<<<<< Updated upstream
     size: 0,
     cs_arr: '',
+=======
+    size: 0,// 7
+    cs_arr: '',// 23, 45, 16, 37, 3, 99, 22
+>>>>>>> Stashed changes
     in_arr: [],
     exe_arr: [],
     left: 0,
@@ -47,9 +40,12 @@ class App extends Component {
     this.setState({
       err_msg,
       in_arr,
+<<<<<<< Updated upstream
       exe_arr: in_arr,
       right: (size - 1),
       pivot: (size - 1)
+=======
+>>>>>>> Stashed changes
     });
   }
 
@@ -57,6 +53,7 @@ class App extends Component {
     // e.target.name
     this.setState({ [e.target.name]: e.target.value });
   }
+<<<<<<< Updated upstream
 
   quickSort = (arr, start, end) => {
     if (start < end) {
@@ -90,11 +87,124 @@ class App extends Component {
     this.setState({
       exe_arr
     })
+=======
+  
+  updateStateAsync = (arr, left, right, pivot, isSwap) => {
+    return new Promise((resolve) => {
+    
+    if(isSwap) swap(arr, left, right);
+      // setTimeout(() => {
+        this.setState({
+          arr,
+          left,
+          right,
+          pivot
+        }, () => {
+        // this.state.arr
+          setTimeout(() => {
+            resolve();
+          }, 3 * 1000)
+        })
+        // resolve(value);
+      // }, 3 * 1000);//Math.floor(Math.random() * 10 * 1000)
+    });
   }
 
-  executeOneStep = () => {
-    this.count_resume++;
-    this.executeAll();
+  quickSortAsync = async (arr, start, end) => {
+    // let { sortedEl } = this.state;
+    if (start < end) {
+      let pivot = await this.partitionAsync(arr, start, end);
+      this.quickSortAsync(arr, start, pivot - 1);
+      this.quickSortAsync(arr, pivot + 1, end);
+    }
+  }
+
+  partitionAsync = (arr, start, end) => {
+    return new Promise(r => {
+
+      let pivot = end;
+      let left = start;
+      let right = end - 1;
+  
+      let promises = [];
+      
+  //       for (i = 0; i < arr.length; ++i) {
+  //         promises.push(updateStateAsync(arr[i]));
+  //       }
+        while(left <= right) {
+          if (arr[left] < arr[pivot]) {
+            promises.push(this.updateStateAsync(arr, left, right, pivot, false));
+            left++;
+          } else if (arr[right] > arr[pivot]) {
+            promises.push(this.updateStateAsync(arr, left, right, pivot, false));
+            right--;
+          } else {
+            // promises.push(this.swapAsync(arr, left, right));
+            promises.push(this.updateStateAsync(arr, left, right, pivot, true));
+          }
+        }
+      
+      Promise.all(promises)
+          .then((results) => {
+            // console.log("All done", arr);
+            swap(arr, left, pivot);
+            let { sortedEl } = this.state;
+            sortedEl.push(pivot);
+
+            this.setState({
+              arr,
+              sortedEl
+            }, () => {
+              
+              r(left);
+            })
+          })
+          .catch((e) => {
+              // Handle errors here
+          });
+      // return left;
+    })
+  }
+
+  quickSort = (arr, start, end) => {
+    if (start < end) {
+      let pivot = this.partition(arr, start, end);
+      this.quickSort(arr, start, pivot - 1);
+      this.quickSort(arr, pivot + 1, end);
+    }
+>>>>>>> Stashed changes
+  }
+
+  partition = (arr, start, end) => {
+    let pivot = end;
+    let left = start;
+    let right = end - 1;
+
+    while (left <= right) {
+      if (arr[left] < arr[pivot]) {
+        left++;
+      } else if (arr[right] > arr[pivot]) {
+        right--;
+      } else {
+        swap(arr, left, right);
+      }
+    }
+    swap(arr, left, pivot);
+    return left;
+  }
+  execute = (opt) => {
+    let { in_arr, size } = this.state;
+    let exe_arr = [...in_arr];
+    this.setState({
+      exe_arr: [...in_arr],
+      right: (size - 1),
+      pivot: (size - 1)
+    });
+    if (opt === 'with_animation') this.quickSortAsync(exe_arr, 0, size - 1);
+    else this.quickSort(exe_arr, 0, size - 1);
+    this.setState({
+      exe_arr,
+    })
   }
 
   render() {
@@ -102,7 +212,11 @@ class App extends Component {
     return (
       <div className="qs">
         <QuickSortInput size={size} cs_arr={cs_arr} err_msg={err_msg} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+<<<<<<< Updated upstream
         <QuickSortOutput in_arr={in_arr} exe_arr={exe_arr} executeAll={this.executeAll} executeOneStep={this.executeOneStep} />
+=======
+        <QuickSortOutput {...this.state} execute={this.execute} executeOneStep={this.executeOneStep} />
+>>>>>>> Stashed changes
       </div>
     );
   }
--- src/components/QuickSortOutput.js
+++ src/components/QuickSortOutput.js
@@ -3,7 +3,12 @@ import { Button } from 'reactstrap';
 
 import './QuickSortOutput.scss';
 
+<<<<<<< Updated upstream
 const QuickSortOutput = ({ in_arr, exe_arr, executeAll, executeOneStep }) => {
+=======
+const QuickSortOutput = ({ in_arr, exe_arr, left, right, pivot, execute }) => {
+  // console.log("TCL: QuickSortOutput -> sortedEl", sortedEl)
+>>>>>>> Stashed changes
   return (
     <Fragment>
       <div className="qso">
@@ -29,8 +34,15 @@ const QuickSortOutput = ({ in_arr, exe_arr, executeAll, executeOneStep }) => {
           }
         </div>
       </div>
+<<<<<<< Updated upstream
         {/* <Button type="button" onClick={executeOneStep}>Next</Button> */}
       <Button type="button" onClick={executeAll}>Run</Button>
+=======
+      <div>
+        <Button className="run-btn" type="button" onClick={() => execute('with_animation')}>Run with animation</Button>
+        <Button className="run-btn" type="button" onClick={() => execute('without_animation')}>Run without animation</Button>
+      </div>
+>>>>>>> Stashed changes
 
     </Fragment>
   )
